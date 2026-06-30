7a444ffe9cbf7fd48bdcf645ed3249e6d65b5c54
Tony Zhou <tonyzhou2016@gmail.com>
2026-06-29 21:17:48 -0400

    server: handle a few options
    Signed-off-by: Tony Zhou <tonyzhou2016@gmail.com>

--- server/main.c
+++ server/main.c
@@ -1,4 +1,69 @@
 
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) { return 0; }
+#include "../common.h"
+
+static const struct option long_opts[];
+static const char *const short_opts;
+void display_help(FILE *, const char *);
+
+#define DEFAULT_ADDR "0.0.0.0"
+
+int main(int argc, char **argv) {
+  int option;
+  int opt_port = DEFAULT_PORT;
+  char *opt_addr = DEFAULT_ADDR;
+  bool opt_fork = false;
+
+  while ((option = getopt_long(argc, argv, short_opts, long_opts, NULL)) !=
+         -1) {
+    switch (option) {
+    case 'h':
+      display_help(stdout, argv[0]);
+      break;
+    case 'v':
+      fprintf(stdout, "dumbos_server version %s versioncode %d.%d.%d\n",
+              VERSION_STRING, VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+      return 0;
+    case 'a':
+      opt_addr = optarg;
+      break;
+    case 'p':
+      opt_port = parse_ushort(optarg);
+      if (opt_port < 0) {
+        print_errno("invalid port", errno);
+        return errno;
+      }
+      break;
+    case 'b':
+      opt_fork = true;
+      break;
+    default:
+      display_help(stderr, argv[0]);
+      return EINVAL;
+    }
+  }
+}
+
+static const struct option long_opts[] = {
+    {"help", no_argument, 0, 'h'},       {"version", no_argument, 0, 'v'},
+    {"port", required_argument, 0, 'p'}, {"address", required_argument, 0, 'a'},
+    {"fork", no_argument, 0, 'b'},       {0, 0, 0, 0}};
+static const char *const short_opts = "hvp:a:b";
+void display_help(FILE *dest, const char *argv0) {
+  fprintf(dest,
+          "Usage:%s [options] <dir> [persist dir]\n"
+          " dir is the directory that holds all the one time secret codes. "
+          "persist dir is the directory with all the persistant secret codes, "
+          "defaulting to TODO\n"
+          "Options:\n"
+          " -h,--help       display this help text\n"
+          " -v,--version    display version and exit\n"
+          " -a,--address    address to listen to, default TODO\n"
+          " -p,--port       port to listen at, default TODO\n"
+          " -b,--fork       fork to background\n",
+          argv0);
+};
